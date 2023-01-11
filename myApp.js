9a0716f5aaf6ccc2cfc7128d63acdf9c41ca1b6e
Gusberly <gusberlymorillo@gmail.com>
2023-01-11 15:38:30 -0300

    as

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 
+app.listen("https://boilerplate-express-production-db38.up.railway.app/");
+
 app.get("/home", (req, res) => {
   res.send("Hello Express");
 });
